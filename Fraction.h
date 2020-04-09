99718f6dabaad6f38dacf4ee37ce8207734f7923
RuslanShugaipov <58910717+RuslanShugaipov@users.noreply.github.com>
2020-04-10 01:56:41 +0300

    Рациональные числа
    Финальные правки.

--- Fraction.h
+++ Fraction.h
@@ -2,13 +2,13 @@
 #include "StructFraction.h"
 #include "Natural.h"
 #include "Integer.h"
-void InputFraction(struct Fraction&);                 //ввод дроби
-void OutputFraction(struct Fraction);                 //вывод дроби
+void InputFraction(struct Fraction&);                 //ГўГўГ®Г¤ Г¤Г°Г®ГЎГЁ
+void OutputFraction(struct Fraction);                 //ГўГ»ГўГ®Г¤ Г¤Г°Г®ГЎГЁ
 Fraction RED_Q_Q(struct Fraction);                    //Q-1
 void INT_Q_B(Fraction);                               //Q-2
 Fraction TRANS_Z_Q(Integer&);                         //Q-3
 Integer TRANS_Q_Z(Fraction&);                         //Q-4
-Fraction ADD_QQ_Q(Fraction&, Fraction&);              //Q-5
+Fraction ADD_QQ_Q(struct Fraction, struct Fraction);              //Q-5
 Fraction SUB_QQ_Q(struct Fraction, struct Fraction);  //Q-6
 Fraction MUL_QQ_Q(struct Fraction, struct Fraction);  //Q-7
 Fraction DIV_QQ_Q(struct Fraction, struct Fraction);  //Q-8
