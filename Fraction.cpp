6875f54600e1d1b4c3f323b8ece70fafa5f526dd
leti9302 <58954003+leti9302@users.noreply.github.com>
2020-04-16 20:35:05 +0300

    Update Fraction.cpp

--- Fraction.cpp
+++ Fraction.cpp
@@ -1,21 +1,21 @@
-/*Ñåìåíîâ Àëåêñåé
-Øóãàèïîâ Ðóñëàí
-        ãð 9302*/
+/*Семенов Алексей
+Шугаипов Руслан
+        гр 9302*/
 #include <iostream>
 #include "Fraction.h"
 using namespace std;
-//Ââîä äðîáè
+//Ввод дроби
 void InputFraction(struct Fraction &q) {
-	q.num=Input();          //ââîäèì ÷èñëèòåëü (öåëîå ÷èñëî)
-	q.denum=InputNatural(); //ââîäèì çíàìåíàòåëü (íàòóðàëüíîå ÷èñëî)
+	q.num=Input();          //вводим числитель (целое число)
+	q.denum=InputNatural(); //вводим знаменатель (натуральное число)
 }
 //Âûâîä äðîáè
 void OutputFraction(struct Fraction q) {
-	if (q.num.b == 1) cout << '-';         //âûâîäèì ÷èñëèòåëü
+	if (q.num.b == 1) cout << '-';         //выводим числитель
 	for (int i = q.num.n; i >= 0; i--)
 		cout << q.num.A[i];
 	cout << '/';
-	OutNatural(q.denum);                   //âûâîäèì çíàìåíàòåëü
+	OutNatural(q.denum);                   //выводим знаменатель
 }
 //Q-1
 Fraction RED_Q_Q(struct Fraction q) {
@@ -39,8 +39,8 @@ Fraction RED_Q_Q(struct Fraction q) {
 //Q-2
 void INT_Q_B(Fraction Q)
 {
-	if (Q.denum.length == 1 && Q.denum.A[0] == 1)//åñëè â çíàìåíàòåëå åäèíèöà, òî ðàöèîíàëüíîå ÷èñëî öåëîå
-	{                                            //åñëè öåëîå, òî äà, íåöåëîå - íåò
+	if (Q.denum.length == 1 && Q.denum.A[0] == 1)//если в знаменателе единица, то рациональное число целое
+	{                                            //если целое, то да, нецелое - нет
 		cout << "äà\n";
 	}
 	else
@@ -51,27 +51,27 @@ void INT_Q_B(Fraction Q)
 //Q-3
 Fraction TRANS_Z_Q(Integer& B)
 {
-	Fraction q;                                  //ñîçäàåì äðîáíîå ÷èñëî
-	q.denum.length = 1;                          //äëèíà çíàìåíàòåëÿ åäèíèöà
-	q.denum.A = new int[1];                       //êîë-âî öèôð â çíàìåíàòåëå
-	q.denum.A[0] = 1;                            //çíàìåíàòåëü ðàâåí åäèíèöå
-	q.num.A = new int[B.n];                      //âûäåëÿåì ïàìÿòü ïîä ÷èñëèòåëü
+	Fraction q;                                  //создаем дробное число
+	q.denum.length = 1;                          //длина знаменателя единица
+	q.denum.A = new int[1];                       //кол-во цифр в знаменателе
+	q.denum.A[0] = 1;                            //знаменатель равен единице
+	q.num.A = new int[B.n];                      //выделяем память под числитель
 	q.num.b = B.b;
 	q.num.n = B.n;
-	for (int i = 0; i <= q.num.n; i++)            //çàïîëíÿåì ÷èñëèòåëü
+	for (int i = 0; i <= q.num.n; i++)            //заполняем числитель
 		q.num.A[i] = B.A[i];
 	return q;
 }
 //Q-4
 Integer TRANS_Q_Z(Fraction& Q)
 {
-	Integer B;                                   //ñîçäàåì öåëîå ÷èñëî
-	if (Q.denum.length == 1 && Q.denum.A[0] == 1)//ïðîâåðÿåì, ÿâëÿåòñÿ ëè çíàìåíàòåëü åäèíèöåé
+	Integer B;                                   //создаем целое число
+	if (Q.denum.length == 1 && Q.denum.A[0] == 1)//проверяем, является ли знаменатель единицей
 	{
 		B.b = Q.num.b;
 		B.n = Q.num.n;
 		B.A = new int[B.n];
-		for (int i = 0; i <= B.n; i++)            //çàïîëíÿåì öåëîå ÷èñëî
+		for (int i = 0; i <= B.n; i++)            //заполняем целое число
 			B.A[i] = Q.num.A[i];
 		return B;
 	}
@@ -97,13 +97,13 @@ Fraction SUB_QQ_Q(struct Fraction q1, struct Fraction q2) {
 	Fraction temp;
 	Natural D1, D2;
 	Integer Z1, Z2;
-	Natural LCM = LCM_NN_N(q1.denum, q2.denum); //Íàõîäèì ÍÎÊ çíàìåíàòåëÿ ïåðâîé äðîáè è çíàìåíàòåëÿ âòîðîé
-	D1 = DIV_NN_N(LCM, q1.denum);               //Äåëèì ÍÎÊ íà çíàìåíàòåëü ïåðâîé äðîáè
+	Natural LCM = LCM_NN_N(q1.denum, q2.denum); //Находим НОК знаменателя первой дроби и знаменателя второй
+	D1 = DIV_NN_N(LCM, q1.denum);               //Делим НОК на знаменатель первой дроби
 	Z1 = TRANS_N_Z(D1);
-	q1.num = MUL_ZZ_Z(q1.num, Z1);              //óìíîæàåì ÷èñëèòåëü ïåðâîé äðîáè íà ïîëó÷åííîå ÷èñëî
-	D2 = DIV_NN_N(LCM, q2.denum);               //àíàëîãè÷íî äëÿ âòîðîé äðîáè
+	q1.num = MUL_ZZ_Z(q1.num, Z1);              //умножаем числитель первой дроби на полученное число
+	D2 = DIV_NN_N(LCM, q2.denum);               //аналогично для второй дроби
 	Z2 = TRANS_N_Z(D2);
-	q2.num = MUL_ZZ_Z(q2.num, Z2);              //âû÷èòàåì èç ÷èñëèòåëÿ ïåðâîé äðîáè ÷èñëèòåëü âòîðîé
+	q2.num = MUL_ZZ_Z(q2.num, Z2);              //вычитаем из числителя первой дроби числитель второй
 	temp.num = SUB_ZZ_Z(q1.num, q2.num);
 	temp.denum = MUL_NN_N(q1.denum, D1);
 	return temp;
@@ -114,8 +114,8 @@ Fraction MUL_QQ_Q(struct Fraction q1, struct Fraction q2) {
 	Integer Z1, Z2, Z3;
 	Z1 = TRANS_N_Z(q1.denum);
 	Z2 = TRANS_N_Z(q2.denum);
-	temp.num = MUL_ZZ_Z(q1.num, q2.num);     //óìíîæàåì ÷èñëèòåëü ïåðâîé äðîáè íà ÷èñëèòåëü âòîðîé
-	Z3 = MUL_ZZ_Z(Z1, Z2);                   //óìíîæàåì çíàìåíàòåëü ïåðâîé äðîáè íà çíàìåíàòåëü âòîðîé
+	temp.num = MUL_ZZ_Z(q1.num, q2.num);     //умножаем числитель первой дроби на числитель второй
+	Z3 = MUL_ZZ_Z(Z1, Z2);                   //умножаем знаменатель первой дроби на знаменатель второй
 	temp.denum = TRANS_Z_N(Z3);
 	return temp;
 }
@@ -125,10 +125,10 @@ Fraction DIV_QQ_Q(struct Fraction q1, struct Fraction q2) {
 	Integer Z1, Z2, Z3;
 	Z1 = TRANS_N_Z(q1.denum);
 	Z2 = TRANS_N_Z(q2.denum);
-	temp.num = MUL_ZZ_Z(q1.num, Z2);   //óìíîæàåì ÷èñëèòåëü ïåðâîé äðîáè íà çíàìåíàòëü âòîðîé
+	temp.num = MUL_ZZ_Z(q1.num, Z2);   //умножаем числитель первой дроби на знаменатль второй
 	if(q1.num.b==q2.num.b) temp.num.b=0;
 	else temp.num.b=1;
-	Z3 = MUL_ZZ_Z(Z1, q2.num);         //óìíîæàåì çíàìåíàòåëü ïåðâîé äðîáè íà ÷èñëèòåëü âòîðîé
+	Z3 = MUL_ZZ_Z(Z1, q2.num);         //умножаем знаменатель первой дроби на числитель второй
 	temp.denum = TRANS_Z_N(Z3);
 	return temp;
 }
